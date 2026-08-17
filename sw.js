35da61ab2d03e95b068436fc238ba4d6df9ffeab
manyimi97 <manyimi97@gmail.com>
2026-08-17 18:50:14 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'dartsnow-v4';
+const FILES = [
+  '/DartsNow/',
+  '/DartsNow/index.html',
+  '/DartsNow/manifest.json',
+  '/DartsNow/icon-192.png',
+  '/DartsNow/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
