59a94d91bb247e646376601b44bb5205456cb52a
manyimi97 <manyimi97@gmail.com>
2026-08-18 16:16:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,20 @@
-const CACHE = 'dartsnow-v5';
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+// v6 - force update
+const CACHE = 'dartsnow-v6';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// Network first - mindig a szerverről tölt, nem cache-ből
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
